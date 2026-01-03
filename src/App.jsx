226e3af3279313467551c9408c3011481f153d14
Favour Emma Effiong <favoureffiong2008@gmail.com>
2026-01-03 16:44:41 +0100

    Update App.jsx
    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import PersonalInformation from "./pages/public/signup/signup.jsx";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/student-portal/personal-information" replace />} />
-      <Route path="/student-portal/personal-information" element={<PersonalInformation />} />
-      <Route path="/student-portal/academic-details" element={<AcademicDetails />} />
-      <Route path="*" element={<Navigate to="/student-portal/personal-information" replace />} />
+      <Route path="/" element={<Navigate to="/pages/public/signup/signup.jsx" replace />} />
+      <Route path="/pages/public/signup/signup.jsx" element={<PersonalInformation />} />
+      <Route path="/signup/signup2.jsx" element={<AcademicDetails />} />
+      <Route path="*" element={<Navigate to="/pages/public/signup/signup.jsx" replace />} />
     </Routes>
   );
 }
