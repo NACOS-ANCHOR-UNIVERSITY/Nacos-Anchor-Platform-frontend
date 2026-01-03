7943c2313fb9e04ba2a9b7c1fa62ee1d5d9036e4
olubusoro <jude59270@gmail.com>
2026-01-03 12:21:18 +0100

    app.jsx edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// src/App.jsx
+
 import {Routes, Route} from "react-router-dom";
 import StudentLayout from "./layouts/StudentLayout";
 import UploadResourcePage from "./features/library/pages/UploadResourcePage";
@@ -6,13 +6,13 @@ import UploadResourcePage from "./features/library/pages/UploadResourcePage";
 function App() {
   return (
     <Routes>
-      {/* All Student Pages use the StudentLayout (and get the Navbar for free) */}
+     
       <Route element={<StudentLayout />}>
         <Route
           path="/student/resources/upload"
           element={<UploadResourcePage />}
         />
-        {/* Add other pages here later, e.g., <Route path="/student/dashboard" ... /> */}
+        
       </Route>
     </Routes>
   );
