7a35b625b7e19df949621c5e4bf322673c4add60
Just-joe924 <theemmanueljohnson@gmail.com>
2026-01-04 16:57:04 +0100

    Admin payment page

--- src/features/admin/payments/index.js
+++ src/features/admin/payments/index.js
@@ -1,4 +1,4 @@
-export { default as AdminTopbar } from "./components/AdminTopbar";
+export { default as AdminTopbar } from "./components/AdminTopBar";
 export { default as StatCards } from "./components/StatCards";
 export { default as PaymentsTable } from "./components/PaymentsTable";
 
