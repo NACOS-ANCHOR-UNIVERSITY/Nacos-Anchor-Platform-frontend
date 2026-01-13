027d9b6f7ef02ea0e5b729f5f90892ce73db48c4
Stephen <praiseadeniji2017@gmail.com>
2026-01-13 16:44:02 +0100

    Update UserManagement.jsx

--- src/pages/admin/UserManagement.jsx
+++ src/pages/admin/UserManagement.jsx
@@ -550,7 +550,7 @@ const UserManagement = () => {
                     type="search"
                     onChange={handleSearch}
                     placeholder="Search by name or matric no"
-                    className="pl-10 pr-4 py-2 border border-[#E5E7EB] rounded-md font-medium text-sm focus:outline-none focus:ring-2 focus:ring-green-500 w-full md:w-[280px]"
+                    className="pl-10 pr-4 py-2 border border-[#E5E7EB] rounded-md font-medium text-sm focus:outline-none focus:ring-2 focus:ring-green-500 w-full md:w-70"
                   />
                 </div>
                 <button className="flex justify-center items-center gap-2 px-4 py-2 border border-[#E5E7EB] rounded-md text-sm font-medium w-full md:w-auto">
@@ -774,7 +774,7 @@ const UserManagement = () => {
             </div>
           </div>
 
-          {/* --- RECENT USER MANAGEMENT LOGS SECTION  --- */}
+          {/* --- RECENT USER MANAGEMENT LOGS SECTION -- */}
           <div className="bg-white rounded-3xl shadow border border-[#E5E7EB] mb-10">
             <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-6 px-6 pt-6 gap-4">
               <h2 className="text-[18px] font-bold text-[#0F172A] flex items-center gap-2">
@@ -838,3 +838,4 @@ const UserManagement = () => {
 };
 
 export default UserManagement;
+
