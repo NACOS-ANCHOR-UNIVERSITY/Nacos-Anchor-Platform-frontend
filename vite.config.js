bfd59ccbcacf4c8a1a39d0044f97267e7e833522
Oluniyisrael <israeloluniyi2004@gmail.com>
2026-01-31 19:00:33 +0100

    Progress

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
         target: "https://nacos.nextgenerationones.org",
         changeOrigin: true,
         secure: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
     },
   },
